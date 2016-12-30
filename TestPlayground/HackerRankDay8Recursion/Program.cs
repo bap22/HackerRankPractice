6103e287301f8c3fdf1a723cf82c66aaa3a29106
Brett Peterson <bpeterson@crestfinancial.com>
2016-12-30 15:27:58 -0700

    day8Recursion

---
+++ TestPlayground/HackerRankDay8Recursion/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+        int num = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"{factorial(num)}");
+
+    }
+    public static int factorial(int number)
+    {
+        if (number != 1)
+        {
+            return number*factorial(number - 1);
+        }
+        else
+        {
+            return number;
+        }
+    }
+}
