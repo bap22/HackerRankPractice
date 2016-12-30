44fb59113d5447886f30f337adb4a23dad4a7f59
Brett Peterson <bpeterson@crestfinancial.com>
2016-12-30 13:26:47 -0700

    day 8 hacker rank

---
+++ TestPlayground/TestPlayground/HackerRankDay8DictionaryPractice.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestPlayground
+{
+    class HackerRankDay8DictionaryPractice
+    {
+        static void Main(string[] args)
+        {
+            bool stillGoing = true;
+            var dictionary = new Dictionary<string, string>();
+            var numEntries = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < numEntries; i++)
+            {
+                var entry = Console.ReadLine().Split(' ');
+                var name = entry[0];
+                var phone = entry[1];
+                dictionary.Add(name,phone);
+            }
+
+            //now print number as many times as a new entry is put in
+            while (stillGoing)
+            {
+                string phoneFound = "";
+                var nameToFind = Console.ReadLine();
+                if (dictionary.TryGetValue(nameToFind, out phoneFound) != null)
+                {
+                    Console.WriteLine($"{nameToFind}={phoneFound}");
+                }
+                else
+                {
+                    Console.WriteLine("Not found");
+                }
+
+            }
+        }
+    }
+}
