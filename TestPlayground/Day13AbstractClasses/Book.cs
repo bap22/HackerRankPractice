d06d0db274df537c45842383f3f01a8c41b3fb4e
Brett Peterson <bpeterson@crestfinancial.com>
2017-01-06 17:17:41 -0700

    day13 abstract

---
+++ TestPlayground/Day13AbstractClasses/Book.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day13AbstractClasses
+{
+    abstract class Book
+    {
+
+        protected String title;
+        protected String author;
+
+        public Book(String t, String a)
+        {
+            title = t;
+            author = a;
+        }
+        public abstract void display();
+
+
+    }
+
+    class MyBook : Book
+    {
+        protected int price;
+        public MyBook(String t, String a, int p): base(t, a)
+        {
+            title = t;
+            author = a;
+            price = p;
+        }
+
+        public override void display()
+        {
+            Console.WriteLine($"Title: {title}");
+            Console.WriteLine($"Author: {author}");
+            Console.WriteLine($"Price: {price}");
+        }
+    }
+}
---
+++ TestPlayground/Day13AbstractClasses/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day13AbstractClasses
+{
+    /// <summary>
+    /// sample input
+    /// The Alchemist
+    /// Paulo Coelho
+    /// 248
+    /// 
+    /// sample output
+    /// Title: The Alchemist
+    /// Author: Paulo Coelho
+    /// Price: 248
+    /// </summary>
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            String title = Console.ReadLine();
+            String author = Console.ReadLine();
+            int price = Int32.Parse(Console.ReadLine());
+            Book new_novel = new MyBook(title, author, price);
+            new_novel.display();
+        }
+    }
+}
