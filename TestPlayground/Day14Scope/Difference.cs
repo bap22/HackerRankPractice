f0ee9f888822e05555a9ed6e59d4517e79833212
Brett Peterson <bpeterson@crestfinancial.com>
2017-01-09 12:02:07 -0700

    scope and linked lists

---
+++ TestPlayground/Day14Scope/Difference.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace Day14Scope
+{
+
+    class Difference
+    {
+        private int[] elements;
+        public int maximumDifference;
+        private int minInt;
+        private int maxInt;
+
+        public Difference(int[] integerArray)
+        {
+            elements = integerArray;
+            maximumDifference = 0;
+            minInt = maxInt = elements[0];
+        }
+
+        public int computeDifference()
+        {
+            foreach (int current in elements)
+            {
+                if (current > maxInt)
+                {
+                    maxInt = current;
+                }
+                if (current < minInt)
+                {
+                    minInt = current;
+                }
+                maximumDifference = maxInt - minInt;
+            }
+            return maximumDifference;
+        }
+    }
+}
---
+++ TestPlayground/Day15LinkedList/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day15LinkedList
+{
+    class Node
+    {
+        public int data;
+        public Node next;
+        public Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+
+    }
+
+    class Program
+    {
+        private static LinkedList<Node> _linkedList = new LinkedList<Node>();
+        private static int listCount = 0;
+
+        static void Main(String[] args)
+        {
+
+            Node head = null;
+            int T = Int32.Parse(Console.ReadLine());
+            while (T-- > 0)
+            {
+                int data = Int32.Parse(Console.ReadLine());
+                head = insert(head, data);
+            }
+            display(head);
+            Console.ReadLine();
+        }
+
+        public static Node insert(Node head, int data)
+        {
+            if (listCount > 0)
+            {
+                LinkedListNode<Node> lastNode = _linkedList.Last;
+                Node nextNode = new Node(data);
+                lastNode.Value.next = nextNode;
+                _linkedList.AddAfter(lastNode, nextNode);
+            }
+            else
+            {
+                head = new Node(data);
+                _linkedList.AddFirst(head);
+            }
+            listCount++;
+            return head;
+        }
+
+        public static void display(Node head)
+        {
+            Node start = head;
+            while (start != null)
+            {
+                Console.Write(start.data + " ");
+                start = start.next;
+            }
+        }
+
+    }
+}
