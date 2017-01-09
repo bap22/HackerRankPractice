a6106d022e3640b0d79e86753a6ff743e815b8c6
Brett Peterson <bpeterson@crestfinancial.com>
2017-01-09 15:13:05 -0700

    interface practice

---
+++ TestPlayground/Day16Exceptions/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day16Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string S = Console.ReadLine();
+            try
+            {
+                int n = Convert.ToInt32(S);
+                Console.WriteLine(n);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Bad String");
+            }
+        }
+    }
+}
---
+++ TestPlayground/Day19Interfaces/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day19Interfaces
+{
+    public interface AdvancedArithmetic
+    {
+        int divisorSum(int n);
+    }
+
+    class Calculator:AdvancedArithmetic
+    {
+        public int divisorSum(int number)
+        {
+            int sum = 0;
+            List<int> factors = new List<int>();
+            int max = (int)Math.Sqrt(number);  //round down
+            for (int factor = 1; factor <= max; ++factor)
+            { //test from 1 to the square root, or the int below it, inclusive.
+                if (number % factor == 0)
+                {
+                    factors.Add(factor);
+                    if (factor != number / factor)
+                    { // Don't add the square root twice
+                        factors.Add(number / factor);
+                    }
+                }
+            }
+            foreach (int i in factors)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Int32.Parse(Console.ReadLine());
+            AdvancedArithmetic myCalculator = new Calculator();
+            int sum = myCalculator.divisorSum(n);
+            Console.WriteLine("I implemented: AdvancedArithmetic\n" + sum);
+        }
+    }
+}
