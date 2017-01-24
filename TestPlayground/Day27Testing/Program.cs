83edba7de19e52e4dcc96164f87c1099f03ae2df
Brett Peterson <bpeterson@crestfinancial.com>
2017-01-24 11:53:33 -0700

    organize code

---
+++ TestPlayground/Day27Testing/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day27Testing
+{
+    class Program
+    {
+        /// <summary>
+        /// must end with YES, NO, YES, NO, YES
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("5");
+            Console.WriteLine("4 3");
+            Console.WriteLine("-1 -3 0 2");
+            Console.WriteLine("5 5");
+            Console.WriteLine("5 -1 1 0 4");
+            Console.WriteLine("6 4");
+            Console.WriteLine("0 -3 -4 -5 2 1");
+            Console.WriteLine("3 3");
+            Console.WriteLine("0 1 -1");
+            Console.WriteLine("7 2");
+            Console.WriteLine("0 -5  6 8 9 10 12");
+        }
+    }
+}
