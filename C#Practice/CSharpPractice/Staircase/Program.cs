bf9a679ff63c3c2b5b613c53473d22f9a7a9a726
Brett Peterson <bpeterson@crestfinancial.com>
2017-02-24 17:00:36 -0700

    a couple quick tests

---
+++ C#Practice/CSharpPractice/Staircase/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Staircase
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int _n;
+            _n = Convert.ToInt32(Console.ReadLine());
+
+            StairCase(_n);
+            Console.ReadLine();
+        }
+
+        static void StairCase(int n)
+        {
+            string pound = "";
+            for (int i = 0; i < n; i++)
+            {
+                pound += "#";
+                Console.WriteLine(pound.PadLeft(n));
+            }
+
+        }
+    }
+}
---
+++ C#Practice/CSharpPractice/SumThemAll/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace SumThemAll
+{
+    class Program
+    {
+        static int summation(int[] numbers)
+        {
+            return numbers.Sum();
+            //in a test, do they want to see it without Linq?
+        }
+
+        static void Main(String[] args)
+        {
+            string fileName = System.Environment.GetEnvironmentVariable("OUTPUT_PATH");
+            TextWriter tw = new StreamWriter(@fileName, true);
+            int res;
+
+            int _numbers_size = 0;
+            _numbers_size = Convert.ToInt32(Console.ReadLine());
+            int[] _numbers = new int[_numbers_size];
+            int _numbers_item;
+            for (int _numbers_i = 0; _numbers_i < _numbers_size; _numbers_i++)
+            {
+                _numbers_item = Convert.ToInt32(Console.ReadLine());
+                _numbers[_numbers_i] = _numbers_item;
+            }
+
+            res = summation(_numbers);
+            tw.WriteLine(res);
+
+            tw.Flush();
+            tw.Close();
+        }
+    }
+}
