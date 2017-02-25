f89a6a4b6c1d2ad667dcab5ee7161c0d43b8e76e
Brett Peterson <bpeterson@crestfinancial.com>
2017-02-25 14:49:09 -0700

    a few more solutions

---
+++ C#Practice/CSharpPractice/AngryChildren/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace AngryChildren
+{
+    /// <summary>
+    /// Bill Gates is on one of his philanthropic journeys to a village in Utopia. He has N packets of candies and would like to distribute one packet to each of the K children in the village (each packet may contain different number of candies). To avoid a fight between the children, he would like to pick K out of N packets such that the unfairness is minimized.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long nNumCandyPackets = long.Parse(Console.ReadLine());
+            long kNumChildren = long.Parse(Console.ReadLine());
+            
+            //init array of candy packets
+            long[] numCandies = new long[nNumCandyPackets];
+            //put the input into the array
+            for (long i = 0; i < nNumCandyPackets; i++)
+            {
+                numCandies[i] = long.Parse(Console.ReadLine());
+            }                
+            //now we have a sorted arry of the candy packets
+            Array.Sort(numCandies);
+
+            //unfairness definition is a summation: https://www.hackerrank.com/challenges/angry-children-2
+            long total = 0;
+            long unFairness = 0;
+            long finalTallyUnfairness = 0;
+
+            for (long i = 0; i < kNumChildren; i++)
+            {
+                unFairness += ((numCandies[i] * i) - total);
+                total += numCandies[i];
+            }
+
+            finalTallyUnfairness = unFairness;
+
+
+            for (long i = kNumChildren; i < nNumCandyPackets; i++)
+            {
+                total -= numCandies[i - kNumChildren];
+                unFairness -= (total - (numCandies[i - kNumChildren] * (kNumChildren - 1)));
+
+                unFairness += ((numCandies[i] * (kNumChildren - 1)) - total);
+                total += numCandies[i];
+                
+                //we want to minimize unfairness, so if this unfairness is less than current FinalUnfairness...
+                if (unFairness < finalTallyUnfairness)
+                {
+                    finalTallyUnfairness = unFairness;
+                }
+            }
+
+            Console.WriteLine(finalTallyUnfairness);
+
+        }
+    }
+}
---
+++ C#Practice/CSharpPractice/PartitionQuickSort/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Solution
+{
+    static void partition(int[] array)
+    {
+        int pivot = array[0];
+        List<int> left = array.Where(x => x < pivot).ToList();
+        List<int> right = array.Where(x => x > pivot).ToList();
+
+        left.Add(pivot);
+        left.AddRange(right);
+
+        Console.WriteLine(String.Concat(left.Select(x => x.ToString() + " ")));
+    }
+
+    static void Main(String[] args)
+    {
+        //a more efficient way?
+        //Console.ReadLine();
+        //Partition(Console.ReadLine().Trim().Split(' ').Select(x => Int32.Parse(x)).ToArray());
+
+        int _ar_size;
+        _ar_size = Convert.ToInt32(Console.ReadLine());
+        int[] _ar = new int[_ar_size];
+        String elements = Console.ReadLine();
+        String[] split_elements = elements.Split(' ');
+        for (int _ar_i = 0; _ar_i < _ar_size; _ar_i++)
+        {
+            _ar[_ar_i] = Convert.ToInt32(split_elements[_ar_i]);
+        }
+
+        partition(_ar);
+    }
+}
